162d57fe2eba202065e0e5adef0f124e9c89dcbf
realTobby <tkattanek00@gmail.com>
2021-06-07 14:30:03 +0200

    Stuff
    demo, userinput, calculator

---
+++ MeMeZehPlusPlus/Calculator/calculator.cpp
@@ -0,0 +1,58 @@
+#include "iostream"
+using namespace std;
+
+int addition(int a, int b)
+{
+	return a + b;
+}
+
+int substract(int a, int b)
+{
+	return a - b;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+int divide(int a, int b)
+{
+	return a / b;
+}
+
+int main()
+{
+    int x, y;
+	int sum;
+	cout << "First number: ";
+	cin >> x;
+	cout << "Second number: ";
+	cin >> y;
+	
+	char o;
+	cout << "Operator? <A>ddition, <S>ubstract, <M>ultiply, <D>ivide : ";
+	cin >> o;
+	
+	switch(o)
+	{
+		case 'a':
+			sum = addition(x, y);
+		break;
+		case 's':
+			sum = substract(x, y);
+		break;
+		case 'm':
+			sum = multiply(x, y);
+		break;
+		case 'd':
+			sum = divide(x, y);
+		break;
+	}
+	
+	cout << "Result is: " << sum;
+    return 0;
+}
+
+
+
